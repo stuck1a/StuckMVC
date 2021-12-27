8f6689b5320e70cf8abe030ec3dabe6fa48da650
stuck1a <60481324+stuck1a@users.noreply.github.com>
2021-12-27 22:49:46 +0100

    composer packages

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a585d243835b2bbcf4b0b619c0bf299c591a612c',
+    'name' => 'stuck1a/stucksseiten',
+  ),
+  'versions' => 
+  array (
+    'smarty/smarty' => 
+    array (
+      'pretty_version' => 'v3.1.40',
+      'version' => '3.1.40.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d4f8309ed49702e0d7152f9983c3a9c4b98eb9d',
+    ),
+    'stuck1a/stucksseiten' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a585d243835b2bbcf4b0b619c0bf299c591a612c',
+    ),
+  ),
+);
